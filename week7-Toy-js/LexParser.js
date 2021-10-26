d0ca38ba782068f593c8be8a55501c5e3046fd34
hheatgithub <tohongho@gmail.com>
2021-10-25 21:54:26 -0500

    week7 toyjs

---
+++ week7-Toy-js/LexParser.js
@@ -0,0 +1,117 @@
+const myXregrexp = {
+  InputElement: "<Whitespace>|<LineTerminator>|<Comments>|<Token>",
+  Whitespace: / /,
+  LineTerminator: /\n/,
+  Comments: /\/\*(?:[^*]|\*[^\/])*\*\/|\/\/[^\n]*/,
+  Token: "<Literal>|<Keywords>|<Identifier>|<Punctuator>",
+  Literal: "<NumericalLiteral>|<BooleanLiteral>|<StringLiteral>|<NullLiteral>",
+  NumericalLiteral: /0x[0-9a-zA-Z]+|0o[0-7]+|0b[01]+|(?:[1-9][0-9]*|0)(?:\.[0-9]*)?|\.[0-9]+/,
+  BooleanLiteral: /true|false/,
+  StringLiteral: /\"(?:[^"\n]|\\[\s\S])*\"|\'(?:[^'\n]|\\[\s\S])*\'/,
+  NullLiteral: /null/,
+  Identifier: /[a-zA-Z_$][a-zA-Z0-9_$]*/,
+  Keywords: /continue|break|if|else|for|function|let|var|new|while/,
+  Punctuator: /\|\||\&\&|\+|\-|\,|\?|\:|\{|\}|\.|\(|\=|\<|\+\+|\=\=|\=\>|\*|\)|\[|\]|;/
+};
+
+class XRegExp {
+  constructor(source, flag, root = "root") {
+    this.table = new Map();
+    this.regexp = new RegExp(this.compileRegExp(source, root, 0).source, flag);
+  }
+
+  compileRegExp(source, name, start) {
+    if (source[name] instanceof RegExp) {
+      // console.log(name, " =", source[name].source);
+      return { source: source[name].source, length: 0 };
+    } else {
+      //console.log(name, " =", source[name]);
+    }
+
+    let length = 0;
+    let regexp = source[name].replace(/\<([^>]+)\>/g, (str, $1) => {
+      this.table.set(start + length, $1);
+      // this.table.set($1, start+ length);
+      ++length;
+      let r = this.compileRegExp(source, $1, start + length);
+      length += r.length;
+      return "(" + r.source + ")";
+    });
+
+    return { source: regexp, length: length };
+  }
+  exec(string) {
+    let r = this.regexp.exec(string);
+    //console.log("-------\nXRegExp -> exec -> r[0]=", r[0]);
+    for (let i = 1; i < r.length; i++) {
+      if (r[i] !== void 0) {
+        //  console.log(this.table.get(i - 1));
+        r[this.table.get(i - 1)] = r[i];
+      }
+    }
+
+    return r;
+  }
+
+  get lastIndex() {
+    return this.regexp.lastIndex;
+  }
+
+  set lastIndex(value) {
+    return (this.regexp.lastIndex = value);
+  }
+}
+
+export function* scan(str) {
+  let regexp = new XRegExp(myXregrexp, "g", "InputElement");
+
+  while (regexp.lastIndex < str.length) {
+    let r = regexp.exec(str);
+
+    if (r.Whitespace) {
+    } else if (r.LineTerminator) {
+    } else if (r.Comments) {
+    } else if (r.NumericalLiteral) {
+      yield {
+        type: "NumericalLiteral",
+        value: r[0]
+      };
+    } else if (r.BooleanLiteral) {
+      yield {
+        type: "BooleanLiteral",
+        value: r[0]
+      };
+    } else if (r.StringLiteral) {
+      yield {
+        type: "StringLiteral",
+        value: r[0]
+      };
+    } else if (r.NullLiteral) {
+      yield {
+        type: "NullLiteral",
+        value: null
+      };
+    } else if (r.Identifier) {
+      yield {
+        type: "Identifier",
+        name: r[0]
+      };
+    } else if (r.Keywords) {
+      yield {
+        type: r[0]
+      };
+    } else if (r.Punctuator) {
+      yield {
+        type: r[0]
+      };
+    } else {
+      throw new Error("unexpected token" + r[0]);
+    }
+
+    if (!r[0].length) {
+      break;
+    }
+  }
+
+  yield { type: "EOF" };
+}
---
+++ week7-Toy-js/SyntaxParser.js
@@ -0,0 +1,173 @@
+import { scan } from "./LexParser.js";
+
+let syntax = {
+  Program: [["StatementList", "EOF"]],
+  StatementList: [["Statement"], ["StatementList", "Statement"]],
+  Statement: [
+    ["ExpressionStatement"],
+    ["IfStatement"],
+    ["WhileStatement"],
+    ["VariableDeclaration"],
+    ["FunctionDeclaration"],
+    ["Block"],
+    ["BreakStatement"],
+    ["ContinueStatement"],
+    ["FunctionDeclaration"]
+  ],
+
+  IfStatement: [["if", "(", "Expression", ")", "Statement"]],
+  VariableDeclaration: [["var", "Identifier", ";"], ["let", "Identifier", ";"]],
+  FunctionDeclaration: [
+    ["function", "Identifier", "(", ")", "{", "StatementList", "}"]
+  ],
+  BreakStatement: [["break", ";"]],
+  ContinueStatement: [["continue", ";"]],
+  Block: [["{", "StatementList", "}"], ["{", "}"]],
+  WhileStatement: [["while", "(", "Expression", ")", "Statement"]],
+  IfStatement: [["if", "(", "Expression", ")", "Statement"]],
+  ExpressionStatement: [["Expression", ";"]],
+  Expression: [["AdditiveExpression"]],
+  AssignmentExpression: [
+    ["LeftHandSideExpression", "=", "LogicalORExpression"],
+    ["LogicalORExpression"]
+  ],
+  LogicalORExpression: [
+    ["LogicalANDExpression"],
+    ["LogicalORExpression", "||", "LogicalANDExpression"]
+  ],
+  LogicalANDExpression: [
+    ["AdditiveExpression"],
+    ["LogicalANDExpression", "&&", "AdditiveExpression"]
+  ],
+  AdditiveExpression: [
+    ["MultiplicativeExpression"],
+    ["AdditiveExpression", "+", "MultiplicativeExpression"],
+    ["AdditiveExpression", "-", "MultiplicativeExpression"]
+  ],
+  MultiplicativeExpression: [
+    ["LeftHandsideExpression"],
+    ["MultiplicativeExpression", "*", "LeftHandsideExpression"],
+    ["MultiplicativeExpression", "/", "LeftHandsideExpression"]
+  ],
+  LeftHandSideExpression: [["CallExpression"], ["NewExpression"]],
+  CallExpression: [
+    ["MemberExpression", "Arguments"],
+    ["CallExpression", "Arguments"]
+  ],
+  Arguments: [["(", ")"], ["ArgumentList", ",", "AssignmentExpression"]],
+  NewExpression: [["MemberExpression"], ["new", "NewExpression"]],
+  MemberExpression: [
+    ["PrimaryExpression"],
+    ["PrimaryExpression", ".", "Identifier"],
+    ["PrimaryExpression", "[", "Expression", "]"]
+  ],
+  PrimaryExpression: [["(", "Expression", ")"], ["Literal"], ["Identifier"]],
+  Literal: [
+    ["NumericalLiteral"],
+    ["StringLiteral"],
+    ["BooleanLiteral"],
+    ["NullLiteral"],
+    ["RegularExpressionLiteral"],
+    ["ObjectLiteral"],
+    ["ArrayLiteral"]
+  ],
+  ObjectLiteral: [["{", "}"], ["{", "PropertyList", "}"]],
+  PropertyList: [["Property"], ["PropertyList", ",", "Property"]],
+  Property: [
+    ["StringLiteral", ":", "AdditiveExpression"],
+    ["Identifier", ":", "AdditiveExpression"]
+  ]
+};
+
+let hash = {};
+
+function closure(state) {
+  hash[JSON.stringify(state)] = state;
+  let queue = [];
+  for (let symbol in state) {
+    if (symbol.match(/^\$/)) {
+      continue;
+    }
+    queue.push(symbol);
+  }
+
+  while (queue.length) {
+    const symbol = queue.shift();
+    // console.log(symbol);
+    if (syntax[symbol]) {
+      for (let rule of syntax[symbol]) {
+        if (!state[rule[0]]) queue.push(rule[0]);
+
+        let current = state;
+        for (let part of rule) {
+          if (!current[part]) current[part] = {};
+          current = current[part];
+        }
+
+        current.$reduceType = symbol;
+        current.$reduceLength = rule.length;
+        //console.log("----symbol=", symbol, " current=", current);
+      }
+    }
+  }
+
+  for (let symbol in state) {
+    if (symbol.match(/^\$/)) {
+      continue;
+    }
+
+    if (hash[JSON.stringify(state[symbol])]) {
+      state[symbol] = hash[JSON.stringify(state[symbol])];
+    } else closure(state[symbol]);
+  }
+}
+let end = { $isEnd: true };
+let start = {
+  Program: end
+};
+
+closure(start);
+console.log("----start", start);
+
+// for (let symbol of scan(source)) {
+//   console.log(symbol);
+// }
+
+export function parse(source) {
+  let stack = [start];
+  let symbolStack = [];
+  function reduce() {
+    let state = stack[stack.length - 1];
+
+    if (state.$reduceType) {
+      let children = [];
+      for (let i = 0; i < state.$reduceLength; i++) {
+        stack.pop();
+        children.push(symbolStack.pop());
+      }
+      return { type: state.$reduceType, children: children.reverse() };
+    } else {
+      console.log("state.$reduceType=", state.$reduceType);
+      debugger;
+      throw new Error("unexpected token");
+    }
+  }
+
+  function shift(symbol) {
+    let state = stack[stack.length - 1];
+
+    if (symbol.type in state) {
+      stack.push(state[symbol.type]);
+      symbolStack.push(symbol);
+    } else {
+      shift(reduce());
+      shift(symbol);
+    }
+  }
+
+  for (let symbol of scan(source)) {
+    shift(symbol);
+  }
+
+  return reduce();
+}
